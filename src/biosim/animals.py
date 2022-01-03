f00b294c1f792a19d5a4f6d43835dbf54b459f10
Sathuriyan Sivathas <sathuriyan.sivathas@nmbu.no>
2022-01-03 15:11:52 +0100

    Added a description for the animals.py

--- src/biosim/animals.py
+++ src/biosim/animals.py
@@ -1,1 +1,6 @@
-#hei
+"""
+This file will contain a main class Animals(Superclass). This Animals class will contains methods for characteristics that are
+common for both herbivores and carnivores. The Animals class will have to subclasses, Herbivores and Carnivores.
+These to subclasses will contain methods specifically for herbivores and carnivores.
+
+"""
