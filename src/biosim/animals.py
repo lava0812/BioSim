504fc6847a8337d17c4a106563c9b16c7a23aa9d
Sathuriyan Sivathas <sathuriyan.sivathas@nmbu.no>
2022-01-03 15:27:11 +0100

    Added the outline for the main class(Animals) and the two subclasses(Herbivores, Carnivores).

--- src/biosim/animals.py
+++ src/biosim/animals.py
@@ -1,6 +1,44 @@
 """
-This file will contain a main class Animals(Superclass). This Animals class will contains methods for characteristics that are
-common for both herbivores and carnivores. The Animals class will have to subclasses, Herbivores and Carnivores.
+This file will contain a main class Animals(Superclass). This Animals class will contains methods for characteristics
+that are common for both herbivores and carnivores. The Animals class will have to subclasses,
+Herbivores and Carnivores.
 These to subclasses will contain methods specifically for herbivores and carnivores.
-
 """
+
+class Animals:
+    """
+    This is the Animals class. This class will contain these methods:
+        - Age
+        - Weight
+        - Fitness
+        - Migration
+        - Birth
+        - Death
+    """
+    pass
+
+
+
+
+
+
+
+
+class Herbivores(Animals):
+    """
+    This is the Herbivores subclass and will contain these methods:
+        - Eat
+    """
+    pass
+
+
+
+
+class Carnivores(Animals):
+    """
+    This is the Carnivores subclass and will contain these methods:
+        - Eat
+        - Kill
+    """
+    pass
+
