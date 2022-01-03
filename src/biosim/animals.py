858817e8d612a661695386a8888abce0dae8b932
Sathuriyan Sivathas <sathuriyan.sivathas@nmbu.no>
2022-01-03 19:18:26 +0100

    Added comments throughout the file.

--- src/biosim/animals.py
+++ src/biosim/animals.py
@@ -16,8 +16,20 @@ class Animals:
         - Migration
         - Birth
         - Death
+
+    Both herbivores and carnivores will have given example parameters, but it must be possible to change these
+    example values to values we prefer. Therefore we need to create a method for changing the parameters that are given
+    in the project description.
     """
-    pass
+    @classmethod
+    def updated_parameters(cls):
+        #Will add something here, when as soon as I define the name of the list that contains the parameter names.
+        pass
+
+
+    def __init__(self):
+
+
 
 
 
