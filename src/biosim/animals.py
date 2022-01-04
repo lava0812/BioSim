094d7a83fe0747abfab636ec8952036926e8a699
Sathuriyan Sivathas <sathuriyan.sivathas@nmbu.no>
2022-01-04 15:19:50 +0100

    Creating a Herbivores class, and creating aging, weight_baby and weight_increase methods.

--- src/biosim/animals.py
+++ src/biosim/animals.py
@@ -7,8 +7,38 @@ These to subclasses will contain methods specifically for herbivores and carnivo
 
 import math
 
+class Herbivores:
+
+    def __init__(self,age = None, weight = None):
+        self.age = 0
+        self.weight = weight
+
+
+    def aging(self):
+        """
+        A method for making the herbivores age. This function will be in the Animals superclass once we create it.
+        """
+        self.age += 1
+
+    def weight_baby(self):
+        """
+        This will use a Gaussian distribution for determining the weight of a Herbivore baby
+        """
+        pass
+
+    def weight_increase(self):
+        """
+
+        """
+
+
+
+
+
+
+"""
 class Animals:
-    """
+    
     This is the Animals class. This class will contain these methods:
         - Age
         - Weight
@@ -20,7 +50,7 @@ class Animals:
     Both herbivores and carnivores will have given example parameters, but it must be possible to change these
     example values to values we prefer. Therefore we need to create a method for changing the parameters that are given
     in the project description.
-    """
+    
     @classmethod
     def updated_parameters(cls):
         #Will add something here, as soon as I define the name of the list that contains the parameter names.
@@ -32,7 +62,7 @@ class Animals:
 
 
 
-
+"""
 
 
 
