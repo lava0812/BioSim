4b546ca44a395f4546716478141e08d3af25a211
Sathuriyan Sivathas <sathuriyan.sivathas@nmbu.no>
2022-01-05 14:37:08 +0100

    Added the necessary tests for animals.py.

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -1,10 +1,43 @@
-
-
 __author__ = "Sathuriyan Sivathas & Lavanyan Rathy"
 __email__ = "sathuriyan.sivathas@nmbu.no & lavanyan.rathy@nmbu.no"
 
 from biosim.animals import Herbivores
 import pytest
 
+
 def test_aging():
     pass
+
+
+def test_weight_baby():
+    pass
+
+
+def test_weight_increase():
+    pass
+
+
+def test_weight_decrease():
+    pass
+
+
+def test_fitness_herbivores():
+    pass
+
+
+def test_death_herbivores():
+    pass
+
+
+def test_birth_herbivore():
+    pass
+
+
+def test_birth_herbivore_probability():
+    pass
+
+
+def test_birth_weight_loss():
+    pass
+
+
