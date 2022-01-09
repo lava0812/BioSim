c1d5e94e5dfc05993829876e721208d2a7c6b5ea
Lavanyan Rathy <lavanyan.rathy@nmbu.no>
2022-01-09 12:46:45 +0100

    simulation_example file

--- src/biosim/simulation_example.py
+++ src/biosim/simulation_example.py
@@ -1,17 +1,21 @@
 from src.biosim.landscape import Landscape, Lowland, Water
+from src.biosim.animals import Animal, Herbivore, Carnivore
 
 
 def simulate(self):
     self.new_fodder()
     self.eat_fodder()
+    self.prey()
     self.death_population()
     self.newborn_herb()
+    self.newborn_carni()
     self.weight_loss()
     self.aging_population()
 
 
 L = Landscape()
 L.herb = [Animal(5, 20) for i in range(50)]
-for i in range(1000):
+L.carni = [Animal(5, 20) for i in range(50)]
+for i in range(500):
     L.simulate()
-    print(len(L.herb), L.fodder)
+    print(len(L.herb), len(L.carni), L.fodder)
