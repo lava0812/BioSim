565a93eaf69df4cf8c63928623eea63fbf1b32aa
Lavanyan Rathy <lavanyan.rathy@nmbu.no>
2022-01-11 17:33:31 +0100

    added the needed test functions to test_island

--- tests/test_island.py
+++ tests/test_island.py
@@ -1,2 +1,29 @@
-def test():
+
+
+__author__ = "Sathuriyan Sivathas & Lavanyan Rathy"
+__email__ = "sathuriyan.sivathas@nmbu.no & lavanyan.rathy@nmbu.no"
+
+
+def test_create_map():
+    pass
+
+def test_map_input():
+    pass
+
+def test_map_boundaries():
+    pass
+
+def test_set_new_parameters():
+    pass
+
+def test_map_lines():
+    pass
+
+def test_population_cell():
+    pass
+
+def test_migrate():
+    pass
+
+def test_annual_cycle():
     pass
