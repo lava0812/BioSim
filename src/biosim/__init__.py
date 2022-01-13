c05ddb860e20b49a6ac65159162e8699239f7a8a
Lavanyan Rathy <lavanyan.rathy@nmbu.no>
2022-01-13 01:20:12 +0100

    added new files: setup.py setup.cfg and pyproject.toml
    also added info on init

--- src/biosim/__init__.py
+++ src/biosim/__init__.py
@@ -1,2 +1,8 @@
-__version__ = "0.0"
-from .animals import Animal
+"""
+Python package for BioSim simulation
+"""
+
+
+__version__ = "3.8"
+__author__ = "Sathuriyan Sivathas & Lavanyan Rathy"
+__email__ = "sathuriyan.sivathas@nmbu.no & lavanyan.rathy@nmbu.no"
